341872bc64622f791c68fe5b7b2aa3f6cf5ff33c
gus.zurita <gzurita@clarika.com.ar>
2019-05-25 22:31:51 -0300

    Add and update modules

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+
+// Components
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    NopagefoundComponent
+  ],
+  exports: [
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
